14557b2ce1f45cd58645102cf6649c48f59e20f7
maksimas <mas1985@mail.ru>
2021-09-19 14:11:44 +0400

    adding simple components

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true
+  images: {
+    domains: ['courses-top.ru']
+  },
+  webpack5(config) {
+    config.module.rules.push({
+      loader: '@svgr/webpack',
+      options: {
+        prettier: false,
+        svgo: true,
+        svgoConfig: {
+          plugins: [{ removeViewBox: false }],
+        },
+        titleProp: true,
+      },
+      test: /\.svg$/,
+    });
+    return config;
+  },
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from "next/head";
+import React from "react";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return <>
+    <Head>
+      <title>Project-NextJS</title>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap" rel="stylesheet" />
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 export default MyApp;
