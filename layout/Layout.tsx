6c670348e8a7a8c9345145d6c8a7ab9f9ea66b59
maksimas <mas1985@mail.ru>
2021-10-23 22:55:06 +0400

    adding context. creating menu layout

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,11 +1,12 @@
 import {LayoutProps} from "./Layout.props";
 import styles from './Layout.module.css';
-// @ts-ignore
-import cn from 'classnames';
 import {Sidebar} from "./Sidebar/Sidebar";
 import {Header} from "./Header/Header";
 import {Footer} from "./Footer/Footer";
 import {FunctionComponent} from "react";
+import {AppContextProvider, IAppContext} from "../context/app.context";
+// @ts-ignore
+import cn from 'classnames';
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
     return (
@@ -20,12 +21,14 @@ const Layout = ({ children }: LayoutProps): JSX.Element => {
     );
 };
 
-export const withLayout = <T extends Record<string, unknown>>(Component: FunctionComponent<T>) => {
+export const withLayout = <T extends Record<string, unknown> & IAppContext>(Component: FunctionComponent<T>) => {
     return function withLayoutComponent(props: T): JSX.Element {
         return (
-            <Layout>
-                <Component {...props}/>
-            </Layout>
+            <AppContextProvider menu={props.menu} firstCategory={props.firstCategory}>
+                <Layout>
+                    <Component {...props}/>
+                </Layout>
+            </AppContextProvider>
         )
     }
 }
