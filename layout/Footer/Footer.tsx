cdb570cfcb9273067cc699cbd97029ff3c5f577b
maksimas <mas1985@mail.ru>
2021-10-23 15:56:16 +0400

    adding footer style and date-fns

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,12 +1,17 @@
 import {FooterProps} from "./Footer.props";
 import styles from './Footer.module.css';
+import {format} from 'date-fns';
 // @ts-ignore
 import cn from 'classnames';
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
     return (
-        <div {...props}>
-            Footer
-        </div>
+        <footer className={cn(className, styles.footer)} {...props}>
+            <div>
+                {format(new Date(), 'yyyy')} - Все права защищены
+            </div>
+            <a href="#" target="_blank">Пользоватльское соглашени</a>
+            <a href="#" target="_blank">Политика безопасности</a>
+        </footer>
     );
 };
