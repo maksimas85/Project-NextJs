5d6a9976109dab38faa046c98fd22241e279085e
maksimas <mas1985@mail.ru>
2021-10-30 21:19:16 +0400

    adding sort reducer

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,21 +1,30 @@
 import {TopPageComponentProps} from "./TopPageCompomemt.props";
-import {Htag, Tag, HhData, Advantages, P} from "../../components";
+import {Advantages, HhData, Htag, Sort, Tag} from "../../components";
 import styles from './TopPageComponent.module.css';
 import {TopLevelCategory} from "../../interfaces/page.interface";
 
 // @ts-ignore
 import cn from 'classnames';
+import {SortEnum} from "../../components/Sort/Sort.props";
+import {useReducer} from "react";
+import {sortReducer} from "./sort.reducer";
 
 export const TopPageComponent = ({ page, products, firstCategory}: TopPageComponentProps): JSX.Element => {
+
+    const [{ products: sortedProducts, sort}, dispatchSort] = useReducer(sortReducer, {products, sort: SortEnum.Rating});
+    const setSort = (sort: SortEnum) => {
+        dispatchSort({type: sort});
+    }
+
     return (
         <div className={styles.wrapper}>
             <div className={styles.title}>
                 <Htag tag='h1'>{page.title}</Htag>
                 {products && <Tag color='gray' size='m'>{products.length}</Tag>}
-                <span>Сортировка</span>
+                <Sort sort={sort} setSort={setSort}/>
             </div>
             <div>
-                {products && products.map(p => (<div key={p._id}>{p.title}</div>))}
+                {sortedProducts && sortedProducts.map(p => (<div key={p._id}>{p.title}</div>))}
             </div>
             <div className={styles.hhTitle}>
                 <Htag tag='h2'>Вакансии - {page.category}</Htag>
