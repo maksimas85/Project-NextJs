cad4e4cf2ec84c6c035d173b832d7a656e57f28b
maksimas <mas1985@mail.ru>
2021-11-14 17:52:34 +0400

    adding button Up and animation

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -3,6 +3,7 @@ import styles from './Layout.module.css';
 import {Sidebar} from "./Sidebar/Sidebar";
 import {Header} from "./Header/Header";
 import {Footer} from "./Footer/Footer";
+import {Up} from "../components";
 import {FunctionComponent} from "react";
 import {AppContextProvider, IAppContext} from "../context/app.context";
 // @ts-ignore
@@ -17,6 +18,7 @@ const Layout = ({ children }: LayoutProps): JSX.Element => {
                 {children}
             </div>
             <Footer className={styles.footer}/>
+            <Up/>
         </div>
     );
 };
